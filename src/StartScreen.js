09e5f9c09db614fffcc762ebd804dc2a1dd2665f
Pawel Chmielewski <panpawelchmielewski@gmail.com>
2024-03-11 21:36:08 +0100

    Managing with useReducer, Loading questions from fake API

---
+++ src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to The React QUIZ!</h2>
+      <h3>question to test your React mastery</h3>
+      <button>Let`s start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
